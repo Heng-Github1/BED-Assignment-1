e13ea453af16e3ff26c5060e872bb898a63bbaac
MetroBeann <134157419+MetroBeann@users.noreply.github.com>
2024-06-24 16:11:12 +0800

    moved app.js and dbconfig.js to news-api-mvc-db

---
+++ NewsArticle/news-api-mvc-db/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = { 
+    user: "booksapi_user",  
+    password: "123",  
+    server: "localhost",  
+    database: "bed_db",  
+    trustServerCertificate: true, 
+    options: { 
+    port: 1433, 
+    connectionTimeout: 60000, 
+    }, 
+  }; 
