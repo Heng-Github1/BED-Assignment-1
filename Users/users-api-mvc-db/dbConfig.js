f00d23102d8c1583371189ab01ffb1c2e73e83c6
Heng-Github1 <https://github.com/Heng-Github1/2024_BED_Practicals>
2024-06-30 18:02:32 +0800

    initialized project and added a dependency

---
+++ Users/users-api-mvc-db/dbConfig.js
@@ -0,0 +1,11 @@
+module.exports = { 
+    user: "ShibaInu",  
+    password: "Sh1b1iSdo1NgICT",  
+    server: "localhost",  
+    database: "bed_db",  
+    trustServerCertificate: true, 
+    options: { 
+    port: 1433, 
+    connectionTimeout: 60000, 
+    }, 
+  }; 
