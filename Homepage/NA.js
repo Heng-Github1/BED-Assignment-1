f8821483d97f37f357da01869a07e1ab8f0c39b1
glizzygobblrr <S10257120@connect.np.edu.sg>
2024-07-15 18:39:13 +0800

    directory layout change

--- Homepage/NA.js
+++
@@ -1,7 +0,0 @@
-/*back button*/
-document.getElementById("back-link").addEventListener("click", () => {
-    history.go(-1);
-  });
-  const searchInput = document.getElementById('search-input');
-  const searchBtn = document.getElementById('search-btn');
-  
--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-    user: "booksapi_user", // Replace with your SQL Server login username
-    password: "46777", // Replace with your SQL Server login password
-    server: "localhost",
-    database: "BED_Assignment_DB_dan", //Replace with your database name
-    trustServerCertificate: true,
-    options: {
-      port: 1433, // Default SQL Server port
-      connectionTimeout: 60000, // Connection timeout in milliseconds
-    },
-  };
+  user: "booksapi_user", // Replace with your SQL Server login username
+  password: "P@ssw0rd2006", // Replace with your SQL Server login password
+  server: "localhost",
+  database: "blogPosts_db", //Replace with your database name
+  trustServerCertificate: true,
+  options: {
+    port: 1433, // Default SQL Server port
+    connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
+};
